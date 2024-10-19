7dd54ff16be3e7029d07d8b7690f52add63bdbc7
Preeti-dabby <preetidabby00@gmail.com>
2024-10-20 00:06:37 +0530

    fix css

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -2,20 +2,20 @@ const DUMMY_PROJECTS=[
     {
         projectTitle:'SIMS',
         description: 'Student Information Management System, an end to end database focused project for performing CRUD operations on students data.',
-        sourceLink:'#',
+        sourceLink:'https://github.com/Preeti-dabby/SIMS.git',
         
     },
     {
         projectTitle:'Electricity Billing System',
         description: 'A Java based project focuses on the calculation of units consumed during the specified time and the money to be charged by the electricity offices.',
-        sourceLink:'#',
+        sourceLink:'https://github.com/Preeti-dabby/Electricity_Billing_System.git',
        
     },
    
     {
         projectTitle:'Portfolio',
         description: 'A personal portfolio website built with React.js, featuring dynamic content, smooth navigation, and a responsive design to showcase projects & skills.',
-        sourceLink:'#',
+        sourceLink:'https://github.com/Preeti-dabby/Portfolio.git',
        
     },
   
